15b50d053d0a14f644ee0e1e4177181045e442d1
136084599 <90099110+136084599@users.noreply.github.com>
2025-03-07 14:23:39 +0800

    Update Config2.bat

--- Config2.bat
+++ Config2.bat
@@ -1,3 +1,3 @@
 @echo off
-curl "http://push.zsjczh.fun/message?token=AfPb.df-SfVYMZS" -F "title=½Å±¾" -F "message======Çë×¢Òâ£¡£¡======¼ì²âµ½¹Ò»ú¾¯¸æ£¡======" -F "priority=9"
+curl "http://api.com" -F "title=è„šæœ¬" -F "message======è¯·æ³¨æ„ï¼ï¼======æ£€æµ‹åˆ°æŒ‚æœºè­¦å‘Šï¼======" -F "priority=9"
 pause
